3ca55e653c4c25385856c6284a182c262410029a
iceberglca <cyriaquekodia@gmail.com>
2020-02-17 16:43:12 +0000

    v2

---
+++ database/migrations/2020_02_17_161243_add_nomfichier_to_mesure.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNomfichierToMesure extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mesure', function (Blueprint $table) {
+            //
+            $table->text('nomfichier')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mesure', function (Blueprint $table) {
+            //
+            $table->removeColumn('nomfichier');
+        });
+    }
+}
